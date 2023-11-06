21d505f465ddfd58e62cbbdec6216cbdf77f835f
gaurabh kumar jha <108286917+gaurabhkumarjha@users.noreply.github.com>
2023-11-06 16:19:14 +0530

    routes.js

--- asswticab/src/server/routers/routes.js
+++ asswticab/src/server/routers/routes.js
@@ -5,5 +5,6 @@ const controller= require("../controller/controller");
 router.post("/addclientdata", controller.addclientdata);
 router.get("/getclientdetails", controller.getclientdetails);
 router.delete("/deleteclientdata/:id", controller.clientdelete);
-
+router.get("/getclientdetailsbyid/:id", controller.getclientdatabyid);
+router.put("/editclientdata/:id", controller.editclientdetails);
 module.exports= router;
