f3fec0952ddb42919520a7da2aff9e4664b88601
gaurabh kumar jha <108286917+gaurabhkumarjha@users.noreply.github.com>
2023-11-06 16:17:41 +0530

    App.js

--- asswticab/src/App.js
+++ asswticab/src/App.js
@@ -2,11 +2,12 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
-
+import Edit from "./Pages/Edit";
 function App() {
   return (
     <Routes>
       <Route path="/" element = {<Home/>}/>
+      <Route path="/edit/:id" element={<Edit/>}/>
     </Routes>
   );
 }
